df3996f59b5d9857badad0ee07beeec9b2f3a37f
Godstoner <114870329+Godstoner@users.noreply.github.com>
2022-10-04 01:32:51 +0300

    Add files via upload

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,1 +1,22 @@
+# Алгоритми гешування SHA.
+
+import hashlib
+# иніціалізуюча строка
+
+str = "timetostop"
+
+
+# закодировать Python.Engineering с помощью encode()
+# затем отправить SHA256()
+
+result = hashlib.sha256 ( str .encode ())
+
+
+# Выводит эквивалентное шестнадцатеричное значение.
+
+print ( "Шістнадцятирічний еквівалент SHA256:" )
+
+print ( result.hexdigest())
+
+print ( "" )
 
